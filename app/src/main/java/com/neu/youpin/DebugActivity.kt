4a7ea24159cee44b0dc1868b19f76e1a413aac8b
Ainz <46172777@qq.com>
2022-04-13 21:38:41 +0800

    创建调试页面

---
+++ app/src/main/java/com/neu/youpin/DebugActivity.kt
@@ -0,0 +1,11 @@
+package com.neu.youpin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DebugActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debug)
+    }
+}
